15e78a250cc57532a6db2d40b5303b4b638ef883
korobov_o <korobok13@yandex.ru>
2022-11-24 18:24:42 +0300

    вторая задача. думаем

---
+++ HomeWork042/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число : ");
+var num = Convert.ToInt32(Console.ReadLine());
+
+int SumNum(in num)
+{
+   while (num > 0)
+   {
+        int num1 = num % 10;
+   }
+}
+
+int num2 = num / 10;
+int num = num - num2;
+int result = 
+i = 0;
+
+
